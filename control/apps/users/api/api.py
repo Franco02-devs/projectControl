18f4c6abfdf4b24c76b0f3fccaf62dec540343a5
Franco <francocahuasoto@gmail.com>
2025-03-01 13:05:21 -0500

    second commit:First model and serializer

---
+++ control/apps/users/api/api.py
@@ -0,0 +1,13 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from apps.users.models import User
+from apps.users.api.serializers import UserSerializer
+from rest_framework.decorators import api_view
+
+@api_view(['GET','POST'])
+def user_api_view(request):
+    if request.method == "GET":
+        users=User.objects.all()
+        users_serializer = UserSerializer(users,many=True)
+        return Response(users_serializer.data)
+        
